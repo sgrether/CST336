063555988433aceb9154d0de37fd4ad8a4c01be2
Scott Grether <sgrether@csumb.edu>
2017-08-31 22:57:44 +0000

    fixed links

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-Nothing here
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8" />
+        <title> Scott Grether: Index </title>
+    </head>
+    
+    <body>
+        <nav>
+            <hr width="50%" />
+            <a href="Portfolio/index.html">Portfolio</a>
+        </nav>
+    </body>
+</html>
