fb0a7f743d73397e9eb357685f058b2e2bac88d0
Scott Grether <sgrether@csumb.edu>
2017-11-10 00:13:55 +0000

    Finished Hangman

--- index.php
+++ index.php
@@ -11,7 +11,8 @@
         <a href="Lab5/index.php">Lab5</a>
         <a href="Lab6/index.php">Lab6</a>
         <a href="Portfolio/index.php">Portfolio</a>
-        <a href="practice/index.php">Practice</a>
+        <a href="practice/index.html">Practice</a>
         <a href="slotmachine/index.php">SlotMachine</a>
+        <a href="Hangman/index.html">Hangman</a>
     </body>
 </html>
